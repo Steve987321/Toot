a4d4d7d37f1ffbefb48e257a6d4403b36e7fe4cd
vierkant <Stevebudding123@gmail.com>
2025-08-10 15:09:17 +0200

    Cmake generator script use relative paths

--- Scripts/cmake/cmake_project.lua
+++ Scripts/cmake/cmake_project.lua
@@ -21,7 +21,6 @@ local cmake = p.modules.cmake
 cmake.project = {}
 local m = cmake.project
 
-
 function m.getcompiler(cfg)
 	local default = iif(cfg.system == p.WINDOWS, "msc", "clang")
 	local toolset = p.tools[_OPTIONS.cc or cfg.toolset or default]
@@ -85,18 +84,20 @@ function m.generate(prj)
 		end
 
 		-- output dir
+
+		local output_dir = project.getrelative(cfg.project, cfg.buildtarget.directory)
 		_p(1,'set_target_properties("%s" PROPERTIES', prj.name)
 		_p(2, 'OUTPUT_NAME "%s"', cfg.buildtarget.basename)
-		_p(2, 'ARCHIVE_OUTPUT_DIRECTORY "%s"', cfg.buildtarget.directory)
-		_p(2, 'LIBRARY_OUTPUT_DIRECTORY "%s"', cfg.buildtarget.directory)
-		_p(2, 'RUNTIME_OUTPUT_DIRECTORY "%s"', cfg.buildtarget.directory)
+		_p(2, 'ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/%s', output_dir)
+		_p(2, 'LIBRARY_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/%s', output_dir)
+		_p(2, 'RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/%s', output_dir)
 		_p(1,')')
 		_p('endif()')
 
 		-- include dirs
 		_p('target_include_directories("%s" PRIVATE', prj.name)
 		for _, includedir in ipairs(cfg.includedirs) do
-			_x(1, '$<$<CONFIG:%s>:%s>', cmake.cfgname(cfg), includedir)
+			_x(1, '$<$<CONFIG:%s>:${CMAKE_CURRENT_SOURCE_DIR}/%s>', cmake.cfgname(cfg), project.getrelative(cfg.project, includedir))
 		end
 		_p(')')
 
@@ -181,11 +182,13 @@ function m.generate(prj)
 			standard["C++14"] = 14
 			standard["C++17"] = 17
 			standard["C++20"] = 20
+			standard["C++23"] = 23
 			standard["gnu++98"] = 98
 			standard["gnu++11"] = 11
 			standard["gnu++14"] = 14
 			standard["gnu++17"] = 17
 			standard["gnu++20"] = 20
+			standard["gnu++23"] = 23
 
 			local extentions = iif(cfg.cppdialect:find('^gnu') == nil, 'NO', 'YES')
 			local pic = iif(cfg.pic == 'On', 'True', 'False')
